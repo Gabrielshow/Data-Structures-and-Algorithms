4f9e2a011edb65861a20c404d994a54897d8892d
Gabrielshow <adefelashogbanmu@gmail.com>
2023-08-13 00:06:51 +0100

    adding files

---
+++ insertion_sort.py
@@ -0,0 +1,31 @@
+#insertion sort
+def insertion_sort(arr):
+    for j in range(1, len(arr)):
+        key = arr[j]
+        #insert a[j] into the sorted sequence arr[1..j-1]
+        i = j - 1
+        while i >= 0 and arr[i] > key:
+            arr[i + 1] = arr[i]
+            i = i - 1
+            
+        arr[i + 1] = key
+        
+    return arr
+
+array_to_sort = [9, 0, 4, 7, 8]
+print(insertion_sort(array_to_sort))
+
+#Rewrite the INSERTION-SORT procedure to sort into nonincreasing instead of nondecreasing order.
+def insertion_sort_right(arr):
+    for j in range(1, len(arr)-1):
+        key = arr[j]
+        i = j - 1
+        while i >= 0 and arr[i] < key:
+            arr[i + 1] = arr[i]
+            i = i + 1
+            
+        arr[ i + 1] = key
+        
+    return arr
+
+print(insertion_sort_right(array_to_sort))
---
+++ merge_sort.py
@@ -0,0 +1,46 @@
+#merge sort
+
+def sort(arr):
+    for i in range(len(arr)):
+        for j in range(1+i, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
+
+
+
+array = [4, 7, 1,3]
+
+print(sort(array))
+def merge(arr, p ,q, r):
+    #arr is the array to be given
+    #p is the lower bound of the array
+    #q is the middle integer of the array
+    #r is the upper bound of the array
+    #we assume p <= q < r
+    #we assume that the arr and its subarrays are sorted
+    q = int(len(arr)/2)
+    n1 = q - p + 1
+    n2 = r - q
+    arrL = arr[:n1]
+    arrR = arr[n1+1:n2]
+    for i in range(1, n1):
+        arrL[i] = arr[p + i -1]
+    for j in range(1, n2):
+        arrR[j] = arr[q + j]
+    #arrL[n1 + 1] = inf
+    #arrR[n2 + 1] = inf
+    i = 1
+    j = 1
+    k = p
+    for k in range(r):
+        if arrL[i] <= arrR[j]:
+            arr[k] = arrL[i]
+            i = i + 1
+        else:
+            arr[k] = arrR[j]
+            j = j + 1
+    return arr
+
+
+        
